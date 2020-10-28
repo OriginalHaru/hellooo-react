7431865645fa2659c60195315ef4aedcbc605f4c
joaquin <Jokongiordano@gmail.com>
2020-10-28 16:14:35 -0300

    finish him

---
+++ public/precache-manifest.9222ddad4ad42785fcf7451487df0a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f839a7e5cf6478edcfc8974d37e84599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7108e6bef6d9f405517",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "d7108e6bef6d9f405517",
+    "url": "/static/js/2.dbc7d17f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.dbc7d17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8923565f4d1448b29b30",
+    "url": "/static/js/main.6a4e3ed9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
